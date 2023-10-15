9232220cd88acd8b913d3067fc5dd356dd786ea6
mohasin28 <147034063+mohasin28@users.noreply.github.com>
2023-10-15 16:12:08 +0530

    Add files via upload
    StarBucks UI page

---
+++ script.js
@@ -0,0 +1,8 @@
+function imgSlider(anything) {
+  document.querySelector(".Starbucks").src = anything;
+}
+
+function changeCircleColor(color) {
+  const circle = document.querySelector(".cricle");
+  circle.style.background = color;
+}
